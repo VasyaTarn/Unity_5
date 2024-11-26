0a9abe1e477112c95aa3e5eebc7a8efa1cd89852
VasyaTarn <vasatopktoneverittomuvlob@gmail.com>
2024-11-26 12:37:34 +0200

    1

---
+++ Assets/Scripts/Unit/UnitMover.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public abstract class UnitMover : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private float _speed;
+    
+    protected virtual void Move()
+    {
+        _rigidbody2D.velocity = -transform.right * _speed;
+    }
+    
+}
